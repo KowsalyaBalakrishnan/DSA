1be4f8f79d66a6893d183152f9c4ad5939cf6eaa
c2220081 <kowsalya_balakrishnan@apple.com>
2024-02-10 21:37:24 +0530

    Intersection Operation on Arrays

--- BasicDSA/src/array/setoperations/intersection/SortedArrayIntersection.java
+++ BasicDSA/src/array/setoperations/intersection/SortedArrayIntersection.java
@@ -1,4 +1,48 @@
 package array.setoperations.intersection;
 
 public class SortedArrayIntersection {
+
+    public static void main(String[] args) {
+
+        int[] a = {2, 3, 6, 8, 9, 17, 19};
+        int[] b = {6, 8, 9, 17, 19};
+
+        int[] c = new int[a.length + b.length];
+
+        int low = 0;
+        int high = b.length - 1;
+        int k = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            int index = binarySearchInB(b, low, high, a[i]);
+            if (index != -1) {
+                c[k] = b[index];
+                k++;
+            }
+        }
+
+        for (int data : c) {
+            System.out.print(data + " ");
+        }
+    }
+
+    private static int binarySearchInB(int[] arr, int low, int high, int element) {
+        int result = -1;
+
+        if (low <= high) {
+            int mid = (low + high) / 2;
+            if (element == arr[mid]) {
+                result = mid;
+                return result;
+            } else if (element < arr[mid]) {
+                high = mid - 1;
+                return binarySearchInB(arr, low, high, element);
+            } else if (element > arr[mid]) {
+                low = mid + 1;
+                return binarySearchInB(arr, low, high, element);
+            }
+        }
+
+        return result;
+    }
 }
--- BasicDSA/src/array/setoperations/intersection/UnsortedArrayIntersection.java
+++ BasicDSA/src/array/setoperations/intersection/UnsortedArrayIntersection.java
@@ -1,4 +1,30 @@
 package array.setoperations.intersection;
 
 public class UnsortedArrayIntersection {
+
+    public static void main(String[] args) {
+
+        int[] a = {9, 17, 3, 2, 6};
+        int[] b = {17, 8, 6, 17, 9};
+
+        int[] c = new int[a.length + b.length];
+        int k = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    c[k] = a[i];
+                    k++;
+                    break;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < c.length; i++) {
+            System.out.print(c[i] + " ");
+        }
+
+
+    }
 }
